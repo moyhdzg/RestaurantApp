1b4c8c27801a9d1e7e7bcde2ca0dfa455c1f50ff
Moises Hdz <moy.hdz.g@gmail.com>
2023-03-02 22:50:18 -0800

    1. Added edit and delete buttons in reservaciones, 2. added the function to delete items, and begun to get data to edit

--- RestaurantApp/src/components/Reservaciones.jsx
+++ RestaurantApp/src/components/Reservaciones.jsx
@@ -3,10 +3,12 @@ import Header from './Header'
 import Footer from './Footer'
 import { useState,useEffect } from 'react';
 import { db } from "../Firebase/firebase";
-import { collection, getDocs, addDoc, doc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, deleteDoc, getDoc } from "firebase/firestore";
+import trash from './imgs/trash.svg';
+import edit from './imgs/edit.svg';
 
 const Reservaciones = () => {
-  const [users,setuser]= useState([])
+  const [users,setUser]= useState([])
   const useCollectionRef= collection(db,'reservacionesMochomo')
   const [name, setName]= useState("")
   const [lastname, setLastname]= useState("")
@@ -27,8 +29,28 @@ const Reservaciones = () => {
 
   const getUsers = async() =>{
     const data = await getDocs(useCollectionRef)
-    console.log(data)
-    setuser(data.docs.map((doc) =>({...doc.data(), id: doc.id})))
+    //console.log(data)
+    setUser(data.docs.map((doc) =>({...doc.data(), id: doc.id})))
+  }
+
+  const deleteUser = async(id) =>{
+    const userDoc = doc(db,'reservacionesMochomo', id);
+    await deleteDoc(userDoc)
+    getUsers()
+  }
+
+  const clickEdit = async (id)=> {
+    //const userDoc = doc(db,'reservacionesMochomo', id);
+    //console.log(userDoc.docs)
+    const docRef = doc(db, "reservacionesMochomo", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    } 
   }
 
   useEffect(()=>{
@@ -44,7 +66,8 @@ const Reservaciones = () => {
         <input type="text" placeholder="Correo" onChange={(e) =>{setCorreo(e.target.value)}} />
         <input type="text" placeholder="Teléfono" onChange={(e) =>{setPhone(e.target.value)}} />
         <input type="number" placeholder="Mesa"onChange={(e) =>{setMesa(e.target.value)}} />
-        <button onClick={createUser}>Enviar</button>
+        <button onClick={createUser}>Crear Reservación</button>
+        <button id='updateButton'>Actualizar Reservación</button>
 
         {users.map((item)=>{
             return(
@@ -54,6 +77,12 @@ const Reservaciones = () => {
                     <h1>Correo: {item.Correo}</h1>
                     <h1>Telefono: {item.Telefono}</h1>
                     <h1>Mesa: {item.Mesa}</h1>
+                    <button id='editButton' onClick={ () =>clickEdit(item.id)}>
+                      <img src={edit} alt="Editar" id='editIcon' />
+                    </button>
+                    <button id='deleteButton' onClick={ () => deleteUser(item.id)}>
+                      <img src={trash} alt="Eliminar" id='trashIcon' />
+                    </button>
                 </div>
             )
         })}
